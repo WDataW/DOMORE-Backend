e61798f561bb582c6e374222bc55af7796539e8b
WdataW <wael272726@gmail.com>
2026-09-19 02:09:41 +0300

    add account rate-limiters

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,17 +1,27 @@
 const { updateFullname, getAllSettings, editSettings, uploadPFP, removePFP, getPFP } = require('@root/controllers');
 const { deleteAccount, logout } = require('../controllers');
 const { getInbox, markMessageAsRead } = require('../controllers/inboxControllers');
+const createLimiter = require('../utils/createLimiter');
+const { minute } = require('../utils/time');
+const looseLimiter = createLimiter({
+    windowMs: 15 * minute,
+    max: 150,
+});
+const strictLimiter = createLimiter({
+    windowMs: 15 * minute,
+    max: 10,
+});
 
 const router = require('express').Router();
-router.get('/pfp', getPFP)
-router.post('/pfp', uploadPFP)
-router.delete('/pfp', removePFP)
+router.get('/pfp', looseLimiter, getPFP)
+router.post('/pfp', strictLimiter, uploadPFP)
+router.delete('/pfp', strictLimiter, removePFP)
 
-router.get('/inbox', getInbox);
-router.patch('/inbox/read/:messageId', markMessageAsRead);
+router.get('/inbox', looseLimiter, getInbox);
+router.patch('/inbox/read/:messageId', looseLimiter, markMessageAsRead);
 
-router.get('/settings', getAllSettings)
-router.patch('/settings', editSettings)
-router.patch('/name', updateFullname)
-router.post('/delete-account', deleteAccount)
+router.get('/settings', looseLimiter, getAllSettings)
+router.patch('/settings', looseLimiter, editSettings)
+router.patch('/name', looseLimiter, updateFullname)
+router.post('/delete-account', strictLimiter, deleteAccount)
 module.exports = router;
