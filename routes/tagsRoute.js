1ccfe3e01c22cd4bc73aa46829b94d54088b1a9c
WdataW <wael272726@gmail.com>
2026-09-19 02:05:59 +0300

    add tags rate-limiter

--- routes/tagsRoute.js
+++ routes/tagsRoute.js
@@ -1,11 +1,18 @@
 const { authenticator } = require('@root/middleware');
 const { getAllTags, createTag, getTag, editTag, deleteTag } = require('@root/controllers');
+const createLimiter = require('../utils/createLimiter');
+const { minute } = require('../utils/time');
 
 const router = require('express').Router();
 
-router.get('/', authenticator, getAllTags)
-router.put('/create-tag', authenticator, createTag)
-router.get('/:tagId', authenticator, getTag)
-router.patch('/:tagId', authenticator, editTag)
-router.delete('/:tagId', authenticator, deleteTag)
+
+const tagsLimiter = createLimiter({
+    windowMs: 15 * minute,
+    max: 150
+});
+router.get('/', authenticator, tagsLimiter, getAllTags)
+router.put('/create-tag', authenticator, tagsLimiter, createTag)
+router.get('/:tagId', authenticator, tagsLimiter, getTag)
+router.patch('/:tagId', authenticator, tagsLimiter, editTag)
+router.delete('/:tagId', authenticator, tagsLimiter, deleteTag)
 module.exports = router;
