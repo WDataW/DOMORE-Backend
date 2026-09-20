60dd8faf8e37ee0c8a3494994deb34a04f5ddd22
WdataW <wael272726@gmail.com>
2026-09-20 05:15:37 +0300

    edit NotFound response

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,9 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 const notFound = async (req, res) => {
     res.status(StatusCodes.NOT_FOUND).json({
-        err: {
-            msg: 'Resource Not Found'
-        }
+        message: 'Resource Not Found'
     });
 }
 module.exports = notFound;
